21aaef875b19c595295102550a92d6b83ca7776c
pratan04 <35501178+pratan04@users.noreply.github.com>
2023-12-12 20:50:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// const superheroes = require('superheroes');
+// var mysuperhero=superheroes.random();
+
+// console.log(mysuperhero);
+
+var toonavatar = require('cartoon-avatar');
+ 
+var cartoon=toonavatar.generate_avatar();
+console.log(cartoon);
